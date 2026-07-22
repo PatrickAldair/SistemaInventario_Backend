f3737ba0911e928aa04e052a568f3ef3ad86e2a9
PatrickAldair <aldairflores1501@gmail.com>
2026-07-22 09:32:07 -0500

    corregir error de cors al eliminar

--- src/main/java/com/example/SIS_INVENTARIO_BACK/security/SecurityConfig.java
+++ src/main/java/com/example/SIS_INVENTARIO_BACK/security/SecurityConfig.java
@@ -31,6 +31,7 @@ public class SecurityConfig {
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/api/auditoria/**", "/api/usuarios/**", "/api/productos/*/stock").hasAuthority("ADMIN")
                         .anyRequest().authenticated()
