8fe48c0e3cc4998a6fcabcb9b69f55110fc92e18
PatrickAldair <aldairflores1501@gmail.com>
2026-07-22 08:59:54 -0500

    arreglo de 403

--- src/main/java/com/example/SIS_INVENTARIO_BACK/security/SecurityConfig.java
+++ src/main/java/com/example/SIS_INVENTARIO_BACK/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.SIS_INVENTARIO_BACK.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -44,7 +45,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://sistema-inventario-frontend-rouge.vercel.app"));
+        configuration.setAllowedOrigins(List.of("https://sistema-inventario-frontend-rouge.vercel.app", "http://localhost:4200"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
